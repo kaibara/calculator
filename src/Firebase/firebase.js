40697ebe85f94e8f588a50edb64607d62139657b
Takumi Kaibara <kaibara.miraidenshi@gmail.com>
2019-02-03 21:15:48 +0900

    fix: return KEYS

--- src/Firebase/firebase.js
+++ src/Firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const config = {
-   apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-   databaseURL: process.env.REACT_APP_DATABASEURL,
-   projectId: process.env.REACT_APP_PROJECTID,
-   storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID
+   apiKey: "AIzaSyBWx4XW1s0Y4UgDneZL8V0ZFYr1Sc4YKvk",
+   authDomain: "myself-calculator.firebaseapp.com",
+   databaseURL: "https://myself-calculator.firebaseio.com",
+   projectId: "projectId",
+   storageBucket: "myself-calculator.appspot.com",
+   messagingSenderId: "1043096824390"
 }
 
 firebase.initializeApp(config);
